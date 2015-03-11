c003401162beb685ffadefcf4263312cd67c9946
Becky Hamm <becky@metricslice.com>
2015-03-11 19:20:33 -0400

    add comments

--- js/router.js
+++ js/router.js
@@ -9,16 +9,23 @@ var Router = Backbone.Router.extend({
   },
 
   initialize: function() {
+    // add new track collection
     this.tracks = new TrackCollection();
+
+    // add track view
     this.tracksView = new TrackCollectionView({
       collection: this.tracks
     });
 
+    // add nav
     this.navView = new NavView();
 
+    // add search box
     this.searchView = new SearchView({
       el: $(".search-view")
     });
+
+    // add genre sidebar
     this.genreSidebarView = new GenreSidebarView({
       el: $(".genre-list")
     });
@@ -27,20 +34,27 @@ var Router = Backbone.Router.extend({
 
     $body = $("body");
 
+    // add tracksview parent element
     $body.append(this.tracksView.el);
 
   },
 
   loadGenre: function(genre) {
+    // render sidebar and nav 
     $body.prepend(this.genreSidebarView.render().el);
     $body.prepend(this.navView.render().el);
 
+    // remove anything in the search box so it doesn't show
     $(".search-view").empty();
 
+    // add a default song list
     this.tracks.loadGenre(genre);
 
+    // listen to when a link is clicked ih the sidebar and filter it
     this.listenTo(this.genreSidebarView, "genre:filter", function(genre) {
       this.tracks.loadGenre(genre);
+
+      // navigate to that url
       this.navigate("genre/" + genre);
 
     });
